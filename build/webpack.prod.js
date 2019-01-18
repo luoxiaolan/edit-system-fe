f58ee3a2cfa77d7b8804001ead293923ff64e6f4
carrie <luoxiaolan@baidu.com>
2019-01-18 16:03:56 +0800

    部署修改
    Change-Id: I3e0195b00cae7d01f2cfd5a37566c3e932d235d7

--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -2,4 +2,137 @@
  * @file webpack 配置
  * @author luoxiaolan@badu.com
  */
-// todo
+const path = require('path');
+const webpack = require('webpack');
+const HtmlwebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const BundleAnalyzer = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const CSSSplitWebpackPlugin = require('css-split-webpack-plugin').default;
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const publicPath = 'assets/edit-system-fe/';
+
+module.exports = {
+    mode: 'production',
+    devtool: false,
+    entry: {
+        index: [path.join(__dirname, '../src/index')]
+    },
+    output: {
+        publicPath: '/',
+        filename: `${publicPath}js/[name]-[contenthash:5].js`,
+        chunkFilename: `${publicPath}js/[name].[contenthash:5].js`
+    },
+    module: {
+        rules:[
+            {
+                test: /\.js[x]?$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.less$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader'
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            plugins: [
+                                require('autoprefixer')({
+                                    browsers: [
+                                        '>1%',
+                                        'last 4 versions',
+                                        'Firefox ESR',
+                                        'not ie < 9' // React doesn't support IE8 anyway
+                                    ]
+                                })
+                            ]
+
+                        }
+                    },
+                    {
+                        loader: 'less-loader',
+                        options: {
+                            javascriptEnabled: true
+                        }
+                    }
+                ],
+                exclude: /\.useable\.less$/
+            },
+            {
+              test: /\.css$/,
+              exclude: /\.useable\.css$/,
+              loader: "style-loader!css-loader"
+            },
+            {
+              test: /\.useable\.css$/,
+              exclude: /node_modules/,
+              loader: "style-loader/useable!css-loader"
+            },
+            {
+                test: /\.(png|jpg|ttf|svg)$/,
+                use: [{
+                    loader: 'url-loader',
+                    options: {
+                        limit: 8192,
+                        name: publicPath + 'img/[name].[hash:8].[ext]'
+                    }
+                }],
+                exclude: /node_modules/
+            }
+        ]
+    },
+    optimization: {
+        minimizer: [
+            new UglifyJsPlugin({
+                cache: true,
+                parallel: true,
+                sourceMap: false,
+                uglifyOptions: {
+                    compress: {
+                        warnings: false
+                    }
+                }
+            }),
+            new OptimizeCSSAssetsPlugin({
+                cssProcessor: require('cssnano')({
+                    reduceIdents: false,
+                    autoprefixer: {
+                        remove: false
+                    }
+                })
+            })
+        ],
+        splitChunks: {
+            chunks: 'all',
+            name: 'vendors'
+        },
+        runtimeChunk: {
+            name: 'runtime'
+        }
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new MiniCssExtractPlugin({
+            filename: publicPath + 'css/[name].[contenthash:8].css',
+            chunkFilename: publicPath + 'css/[id].[contenthash:8].css'
+        }),
+        new webpack.ProvidePlugin({
+            $: 'jquery'
+        }),
+        new CSSSplitWebpackPlugin({
+            size: 4000,
+            filename: publicPath + 'css/[name]-[part].[ext]'
+        }),
+        new HtmlwebpackPlugin({
+            title: 'edit-system-fe',
+            template: path.join(__dirname, '../public/index.html'),
+            filename: './template/edit-system-fe/index.html'
+        }),
+        // new BundleAnalyzerPlugin()
+    ]
+};
