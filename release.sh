9bc1799ac9efc76070d5e69825cd120b54a81dee
luoxiaolan <luoxiaolan@baidu.com>
2019-01-23 16:08:54 +0800

    打包文件修改
    Change-Id: Ia41b1078b72236f319ea05ab750dcc6935c54d15

--- release.sh
+++ release.sh
@@ -7,7 +7,7 @@ fi
 echo "webpack done ! "
 
 cd dist
-tar -czvf fdi-console-fe.tar.gz ./assets ./template
+tar -czvf edit-system-fe.tar.gz ./assets ./template
 rm -rf ./assets ./template
 cd ..
 echo "build to dist done ! "
